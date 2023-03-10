4b7ef10c30c329fb94b86dc146cbd61f0c430c3a
Andres Soibelzon <a.soibelzon@gmail.com>
2023-03-10 19:46:25 +0000

    poniendole algunas funcionalidades y cambiando un poco el css

--- src/api/models.py
+++ src/api/models.py
@@ -4,6 +4,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -14,6 +15,28 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "user_name": self.user_name,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+# esto puede ser un post de noticia propia
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), unique=False, nullable=False)
+    image= db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(120), unique=False, nullable=False)
+    url = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Post {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.id,
+            "image": self.image,
+            "description": self.description,
+            "url": self.url,
         }
--- src/front/js/component/login.jsx
+++ src/front/js/component/login.jsx
@@ -1,8 +1,58 @@
-import React from "react";
-import "../../styles/login.css";
-// style={{ height: '10%' }}
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Login = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [values, setValues] = useState({ showPassword: false });
+  const [showError, setShowError] = useState(false);
+  const navigate = useNavigate();
+  const { actions } = useContext(Context);
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  const handleResetForm = () => {
+    setEmail(""), setPassword("");
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.target.classList.add("was-validated");
+    if (!e.target.checkValidity()) {
+      return;
+    }
+    fetch(process.env.BACKEND_URL + "/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error();
+        }
+        return response.json();
+      })
+      .then((data) => {
+        saveToken(data.access_token);
+        actions.login();
+        navigate("/");
+      })
+      .catch(() => setShowError(true));
+  };
+
   return (
     <section
       className="h-100 gradient-form"
@@ -11,80 +61,71 @@ export const Login = () => {
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-xl-10">
-            <div className="card rounded-3 text-black">
-              <div className="row g-0">
+            <div className="card rounded-3 text-black ">
+              <div className="row g-0 justify-content-around">
                 <div className="col-lg-6">
                   <div className="card-body p-md-5 mx-md-4">
-                    <div className="text-center">
+                    <div className="text-left">
                       {/* <img
                         src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
                         style="width: 185px;"
                         alt="logo"
                       /> */}
-                      <h4 className="mt-1 mb-5 pb-1">
-                        We are The SignalMood Team
-                      </h4>
+                      <h4 className="mt-1 mb-5 pb-1">Log In</h4>
                     </div>
 
-                    <form>
-                      <p>Please login to your account</p>
-
+                    <form
+                      className="needs-validation"
+                      noValidate
+                      onSubmit={onFormSubmit}
+                    >
                       <div className="form-outline mb-4">
+                        <label className="form-label" htmlFor="form-email">
+                          Email
+                        </label>
                         <input
                           type="email"
-                          id="form2Example11"
+                          onChange={(e) => setEmail(e.target.value)}
+                          value={email}
+                          id="form-email"
                           className="form-control"
-                          placeholder="Phone number or email address"
+                          placeholder="Insert an email address"
                         />
-                        <label className="form-label" htmlFor="form2Example11">
-                          Username
-                        </label>
                       </div>
-
+                      <label className="form-label" htmlFor="form-password">
+                        Password
+                      </label>
                       <div className="form-outline mb-4">
                         <input
                           type="password"
-                          id="form2Example22"
+                          id="form-password"
                           className="form-control"
+                          placeholder="••••••"
                         />
-                        <label className="form-label" htmlFor="form2Example22">
-                          Password
-                        </label>
                       </div>
 
-                      <div className="text-center pt-1 mb-5 pb-1">
+                      <div className="text-left pt-1 mb-5 pb-1">
                         <button
-                          className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3"
+                          className="btn btn-primary rounded px-3 "
                           type="button"
                         >
                           Log in
                         </button>
-                        <a className="text-muted" href="#!">
+                        {/* <a className="text-muted" href="#!">
                           Forgot password?
-                        </a>
+                        </a> */}
                       </div>
 
-                      <div className="d-flex align-items-center justify-content-center pb-4">
+                      <div className="d-flex align-items-center pb-4">
                         <p className="mb-0 me-2">Don't have an account?</p>
                         <button
                           type="button"
-                          className="btn btn-outline-danger"
+                          className="btn btn-link"
                         >
                           Create new
                         </button>
                       </div>
                     </form>
-                  </div>
-                </div>
-                <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
-                  <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                    <h4 className="mb-4">We are more than just a company</h4>
-                    <p className="small mb-0">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmod tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
                   </div>
                 </div>
               </div>
