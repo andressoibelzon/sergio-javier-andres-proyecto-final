8559459614ba73cb8f3a7cfb95a6df4d2119a822
javgarmon <javier.garces.m@gmail.com>
2023-03-12 19:24:54 +0000

    primera version de home y su css

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,63 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import carrousel1Url from "../../img/bull&bear.jpeg";
+import homeChartsUrl from "../../img/homeCharts.jpeg";
+
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container-fluid">
+      <div class="container-fluid">
+        <img src={carrousel1Url} className="img-fluid" alt="Imagen" />
+        <div class="texto-superpuesto1">
+          <h1>Título del texto</h1>
+          <p>Texto que quieres poner sobre la imagen.</p>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <img src={homeChartsUrl} className="img-fluid" alt="Imagen" />
+        <div class="texto-superpuesto">
+          <h1>Título del texto</h1>
+          <p>Texto que quieres poner sobre la imagen.</p>
+        </div>
+      </div>
+      <div className="card-group">
+        <div class="card" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">INDICES</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              Esta seccion es la de los indices
+            </h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div class="card" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+        <div class="card" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
