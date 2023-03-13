c84ff588d481db587892990a7d3346a86f64d22d
Andres Soibelzon <a.soibelzon@gmail.com>
2023-03-13 21:02:00 +0000

    agregando componente de logout

---
+++ src/front/js/component/logout.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useContext } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+import "../../styles/login.css";
+
+
+export const Logout = () => {
+  return (
+    <Link to="/">
+    <div className="">
+      <button type="button" className="btn btn-primary">
+        Log out
+      </button>
+    </div>
+    </Link>
+  );
+};
--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -3,11 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/login.css";
 import { LoginCode } from "../component/logincode.jsx";
+import { Logout } from "../component/logout.jsx";
 
 export const Login = () => {
   return (
     <div className="h-100" style={{ backgroundColor: "#eee" }}>
       <LoginCode />
+      <Logout />
     </div>
   );
 };
