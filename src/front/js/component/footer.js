739a2cbc9c2a34fd6dc7b2f32b3e8ac2c2c21916
Pas0ta <ssantamariarodrigo@gmail.com>
2023-03-16 18:14:33 +0000

    primera version footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -39,12 +39,12 @@ export const Footer = () => (
 
 			<div className="col-md-5 offset-md-1 mb-3">
 				<form>
-					<h5>Subscribe to our newsletter</h5>
-					<p>Monthly digest of what's new and exciting from us.</p>
+					<h5>Suscrición</h5>
+					<p>La mejor información de mercados finacieros.</p>
 					<div className="d-flex flex-column flex-sm-row w-100 gap-2">
-						<label for="newsletter1" className="visually-hidden">Email address</label>
+						<label for="newsletter1" className="visually-hidden">Email</label>
 						<input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-						<button className="btn btn-primary" type="button">Subscribe</button>
+						<button className="btn btn-primary" type="button">Suscribete</button>
 					</div>
 				</form>
 			</div>
