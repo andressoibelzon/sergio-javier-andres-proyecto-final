2579a78d0b2dd7ea70e7693292ecbc4ba4ebce2e
javgarmon <javier.garces.m@gmail.com>
2023-03-18 10:26:56 +0000

    primera version feed de noticias

--- src/front/js/pages/noticias.js
+++ src/front/js/pages/noticias.js
@@ -5,5 +5,16 @@ import "../../styles/noticias.css";
 export const Noticias = () => {
   const { store, actions } = useContext(Context);
 
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <iframe
+        id="iframe"
+        width="670"
+        height="800"
+        src="https://www.inoreader.com/stream/user/1005138984/tag/all-articles/view/html?cs=m"
+        frameborder="0"
+        tabindex="-1"
+      ></iframe>
+    </div>
+  );
 };
