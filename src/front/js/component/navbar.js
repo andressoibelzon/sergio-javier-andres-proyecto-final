e227d8126792114e7864dc7597c9774cc2aec894
Andres Soibelzon <a.soibelzon@gmail.com>
2023-03-21 21:14:30 +0000

    condicional en el navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,58 +1,27 @@
 import React, { useState, useContext } from "react";
-import { Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/nav.css";
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
-  const navigate = useNavigate()
-  
-	function handleLogout() {
-		actions.logout()
-		navigate("/")
-	}
+  const navigate = useNavigate();
 
+  function handleLogout() {
+    actions.logout();
+    navigate("/");
+  }
 
   return (
-    // <nav classNameName="navbar navbar-light bg-light">
-    // 	<div classNameName="container">
-    // 		<Link to="/home">
-    // 			<span classNameName="navbar-brand mb-0 h1">Nombre</span>
-    // 		</Link>
-    // 		<Link to="/indices">
-    // 			<button classNameName="btn ml-auto">Indices</button>
-    // 		</Link>
-    // 		<Link to="/noticias">
-    // 			<button classNameName="btn ml-auto">Noticias</button>
-    // 		</Link>
-    // 		<Link to="/graficos">
-    // 			<button classNameName="btn ml-auto">Graficos</button>
-    // 		</Link>
-    // 		<div classNameName="ml-auto">
-    // 			<Link to="/login">
-    // 				<button classNameName="btn btn-primary">login</button>
-    // 			</Link>
-    // 			<Link to="/register">
-    // 				<button classNameName="btn btn-primary">registro</button>
-    // 			</Link>
-    // 		</div>
-    // 	</div>
-    // </nav>
     <nav className="styleNav p-3 text-bg-black">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-       <Link className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none" to="/">
-            <svg
-              className="bi me-2"
-              width="40"
-              height="32"
-              role="img"
-              aria-label="Bootstrap"
-            >
-              <use xlinkHref="#bootstrap"></use>
-            </svg>
-           </Link>
-
+          <Link
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+            to="/"
+          >
+            <h3 className="mx-5">SignalMood</h3>
+          </Link>
 
           <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
@@ -61,52 +30,86 @@ export const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link to="/indices" className="nav-link px-2 text-white">
-                Indices
+            {/* si store.auth esta logueado mostrar indices, graficos, noticias */}
+
+            {store.auth === true ? (
+              <Link to="/noticias">
+                <button
+                  type="button"
+                  onClick={handleLogout}
+                  className="btn btn-outline-light me-2"
+                >
+                  Noticias
+                </button> 
               </Link>
-            </li>
-            <li>
-              <Link to="/noticias" className="nav-link px-2 text-white">
-                Noticias
-              </Link>
-            </li>
-            <li>
-              <Link to="/graficos" className="nav-link px-2 text-white">
+              &&
+              <Link to="/graficos">
+              <button
+                type="button"
+                onClick={handleLogout}
+                className="btn btn-outline-light me-2"
+              >
                 Graficos
-              </Link>
-            </li>
-            <li>
+              </button> 
+            </Link>
+              &&
+              <Link to="/indices">
+              <button
+                type="button"
+                onClick={handleLogout}
+                className="btn btn-outline-light me-2"
+              >
+                Indices
+              </button> 
+            </Link>
+            ) : null
+            }
+
               <Link to="/contacto" className="nav-link px-2 text-white">
                 Contacto
               </Link>
             </li>
           </ul>
 
-          {/* <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-						<input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" />
-					</form> */}
-
-          {/* si store.auth esta logueado mostrar log out, sino alreves */}
+            {/* login registro */}
           <div className="text-end">
+            {/* si store.auth esta logueado mostrar log out, sino alreves */}
             {store.auth === true ? (
               <Link to="/">
-                <button type="button" onClick={handleLogout} className="btn btn-outline-light me-2">
-                  Log out
+                <button
+                  type="button"
+                  onClick={handleLogout}
+                  className="btn btn-outline-light me-2"
+                >
+                  Salir
                 </button>
               </Link>
             ) : (
               <Link to="/login">
                 <button type="button" className="btn btn-outline-light me-2">
-                  Log in
+                  Inicio
                 </button>
               </Link>
             )}
 
-            <Link to="/register">
-              <button type="button" className="btn btn-warning">
-                Sign-up
-              </button>
-            </Link>
+            {/* si store.auth esta logueado mostrar Perfil sino registrarse */}
+            {store.auth === true ? (
+              <Link to="/">
+                <button
+                  type="button"
+                  onClick={handleLogout}
+                  className="btn btn-outline-light me-2"
+                >
+                  Perfil
+                </button>
+              </Link>
+            ) : (
+              <Link to="/register">
+                <button type="button" className="btn btn-outline-light me-2">
+                  Registro
+                </button>
+              </Link>
+            )}
           </div>
         </div>
       </div>
