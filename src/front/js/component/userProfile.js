e7c50ad0b6a42e79688f80c6e0f5f5dabf2c11b0
javgarmon <javier.garces.m@gmail.com>
2023-03-21 20:14:51 +0000

    rutas corregidas

--- src/front/js/component/userProfile.js
+++ src/front/js/component/userProfile.js
@@ -1,9 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../store/appContext";
-import ModalDeleteUser from "../modalDeleteUser.js";
-import { getToken } from "../tokenUtilities.js";
+import ModalDeleteUser from "./modalDeleteUser.js";
+import { getToken, deleteToken } from "../component/../store/tokenUtilities";
 // import { Link } from "react-router-dom";
-import { deleteToken } from "../tokenUtilities.js";
 
 const userProfile = () => {
   const [showModal, setShowModal] = useState(false);
@@ -40,50 +39,51 @@ const userProfile = () => {
   return (
     <>
       <div className="card profile-date mt-4">
+        {" "}
         {/* <img
-          src={store.user.image}
-          className="avatar rounded-circle img-thumbnail img-profile-date img-fluid mt-2 mb-3"
-          alt="..."
-        /> */}
-
+                                              src={store.user.image}
+                                              className="avatar rounded-circle img-thumbnail img-profile-date img-fluid mt-2 mb-3"
+                                              alt="..."
+                                            /> */}{" "}
         <div className="card-body-h5 mb-3">
           <h5 className="card-title fs-1 text-capitalize user">
-            {store.user.user_name}
-          </h5>
-        </div>
+            {" "}
+            {store.user.user_name}{" "}
+          </h5>{" "}
+        </div>{" "}
         <ul className="list-group list-group-flush list-body-main mt-4 rounded">
           <li className="list-group-item text-capitalize list-body">
-            <strong>Nombre</strong>: {store.user.name}
-          </li>
+            <strong> Nombre </strong>: {store.user.name}{" "}
+          </li>{" "}
           <li className="list-group-item text-capitalize list-body">
-            <strong>Primer apellido</strong>: {store.user.first_name}
-          </li>
+            <strong> Primer apellido </strong>: {store.user.first_name}{" "}
+          </li>{" "}
           <li className="list-group-item text-capitalize list-body">
-            <strong>Segundo apellido</strong>: {store.user.last_name}
-          </li>
+            <strong> Segundo apellido </strong>: {store.user.last_name}{" "}
+          </li>{" "}
           <li className="list-group-item list-body">
-            <strong>Correo electrónico</strong>: {store.user.email}
-          </li>
-        </ul>
+            <strong> Correo electrónico </strong>: {store.user.email}{" "}
+          </li>{" "}
+        </ul>{" "}
         <div className="btn-group mt-4  mb-2">
           <button
             className="btn btn-md btn button-dlt bg-gradient rounded"
             type="delete"
             onClick={onDeleteButtonClick}
           >
-            Eliminar cuenta
-          </button>
+            Eliminar cuenta{" "}
+          </button>{" "}
           {/* <Link to="/profile/modificate">
-            <button
-              className="btn btn-md btn btn-profile bg-gradient  ms-4"
-              type="button"
-            >
-              Modificar datos de usuario
-            </button>
-          </Link> */}
-        </div>
-      </div>
-      {showModal && <ModalDeleteUser />}
+                                                <button
+                                                  className="btn btn-md btn btn-profile bg-gradient  ms-4"
+                                                  type="button"
+                                                >
+                                                  Modificar datos de usuario
+                                                </button>
+                                              </Link> */}{" "}
+        </div>{" "}
+      </div>{" "}
+      {showModal && <ModalDeleteUser />}{" "}
     </>
   );
 };
