91f29c476d37835d0d8fe1ce7afd305a1bc4cf1c
Pas0ta <ssantamariarodrigo@gmail.com>
2023-03-24 21:21:37 +0000

    creado imput y botton recoverypassword

--- src/front/js/component/recoverypassword.jsx
+++ src/front/js/component/recoverypassword.jsx
@@ -2,4 +2,14 @@
 
 export const RecoveryPassword = () => {
 
+    return (
+        <div>
+            <form id="reset-password-form">
+                <label for="email">Correo electrónico:</label>
+                <input type="email" id="email" name="email" required />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    )
+
 }
