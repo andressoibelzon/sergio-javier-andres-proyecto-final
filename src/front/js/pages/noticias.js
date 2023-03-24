693527f47f3a290215c6950454aa5700b25dd785
Pas0ta <ssantamariarodrigo@gmail.com>
2023-03-24 18:37:39 +0000

    version 2

--- src/front/js/pages/noticias.js
+++ src/front/js/pages/noticias.js
@@ -6,15 +6,22 @@ export const Noticias = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="container">
-      <iframe
-        id="iframe"
-        width="670"
-        height="800"
-        src="https://www.inoreader.com/stream/user/1005138984/tag/all-articles/view/html?cs=m"
-        frameborder="0"
-        tabindex="-1"
-      ></iframe>
-    </div>
+    <body className="noticias">
+      <div className="w-50 mx-auto">
+        <br />
+        <br />
+        <h1 className="d-flex justify-content-center text-light">últimas notcias</h1>
+        <br />
+        <br />
+        <iframe
+          id="iframe"
+          width="670"
+          height="800"
+          src="https://www.inoreader.com/stream/user/1005138984/tag/all-articles/view/html?cs=m"
+          frameborder="0"
+          tabindex="-1"
+        ></iframe>
+      </div>
+    </body>
   );
 };
