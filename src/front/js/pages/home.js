95c79420f552eb8755311baacee8958c6e490858
Andres Soibelzon <a.soibelzon@gmail.com>
2023-03-25 19:53:45 +0000

    terminado la primera etapa del home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import carrousel1Url from "../../img/bull&bear.jpeg";
 import homeChartsUrl from "../../img/homeCharts.jpeg";
+import { useNavigate, Link } from "react-router-dom";
+
 
 
 import "../../styles/home.css";
@@ -42,18 +44,20 @@ export const Home = () => {
   });
 
   return (
-    <div className="container-fluid py-3">
+    <div className="container-fluid py-5">
 
-      <div id="seccion1" className="container-fluid position-relative">
-        <img src={carrousel1Url} className="img-fluid" alt="bullbear" style={{maxHeight: "600px", height: "auto", width:"100%"}}/>
+      <div id="seccion1" className="container-fluid position-relative ">
+        <img src={carrousel1Url} className="img-fluid  shadow " alt="bullbear" style={{maxHeight: "600px", height: "auto", width:"100%"}}/>
         
-          {/* <h1>
+          <h1 className="position-absolute bottom-50 start-50 translate-middle text-white text-center">
             Tu camino al éxito financiero empieza aquí
-          </h1> */}
+          </h1>
           
-          <button type="button" className="btn btn-outline-primary position-absolute bottom-0 start-50 translate-middle">
+          <Link to="/register">
+          <button type="button" className="btn btn-outline-light position-absolute bottom-0 start-50 translate-middle my-3">
             Ir a registro
           </button>
+          </Link>
         </div>
 
 
@@ -65,20 +69,19 @@ export const Home = () => {
           </button>
         </div> */}
       
+      <hr className="mb-5"></hr>
+        <h2 className="container d-flex justify-content-center mb-5">
+        Analítica de mercados: Gráficos, Noticias e Indices en tiempo real.
+        </h2>
 
-      <div id="titulo_carrusel" className="container">
-        <h1 className="container  d-flex justify-content-center">
-          Todo lo que necesitas, <br /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AQUÍ MISMO
-        </h1>
-      </div>
-      <br />
-      <br />
-      <div>
-        <h2 className="container d-flex justify-content-center"><span className="badge rounded-pill bg-dark"> Gráficos en tiempo real</span></h2>
-      </div>
+<hr className="mb-5"></hr>
+
+{/* esto contiene toda la seccion del grafico */}
+<div className="">
+        <h2 className="container d-flex justify-content-center "> Gráficos en tiempo real</h2>
       <div
         id="container-carrusel"
-        className="container shadow-lg carousel-fade"
+        className="container shadow carousel-fade"
       >
         <div
           id="carouselExampleRide"
@@ -120,12 +123,12 @@ export const Home = () => {
               </iframe>
             </div>
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+          <button className="carousel-control-prev bg-black" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Previous</span>
           </button>
           <button
-            className="carousel-control-next"
+            className="carousel-control-next bg-black"
             type="button"
             data-bs-target="#carouselExampleRide"
             data-bs-slide="next"
@@ -135,14 +138,23 @@ export const Home = () => {
           </button>
         </div>
       </div>
-      <br />
-      <br />
-      <div className="container-fluid">
-        <img src={homeChartsUrl} className="img-fluid" alt="Imagen" />
-        <div className="texto-superpuesto">
-          <h1><span className="badge rounded-pill bg-dark"> Actualizate con las mejores noticias del día</span></h1>
-          <br />
-          <br />
+      </div>
+
+      <hr></hr>
+
+      {/* esto muestra el contenido del indice */}
+      <div className="">
+      <h2 className="container d-flex justify-content-center mb-5"> Indices</h2>
+
+      </div>
+
+      <hr></hr>
+
+{/* esto muestra el contenido de las noticias */}
+      <div className="">
+        {/* <img src={homeChartsUrl} className="img-fluid" alt="Imagen" /> */}
+          <h2 className="container d-flex justify-content-center mb-5"> Actualizate con las noticias del dia</h2>
+
           {/* <p>Texto que quieres poner sobre la imagen.</p> */}
           <iframe // noticias
             className="container rounded d-flex justify-content-center"
@@ -162,8 +174,55 @@ export const Home = () => {
             src="https://darqube.com/external-embedding/market-overview?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ3aWRnZXQiOnsib3ZlcnZpZXdfc2VjdGlvbnMiOltdLCJpbnN0cnVtZW50cyI6eyJJTkRFWEVTIjpbXX19LCJ3X3R5cGUiOiJNYXJrZXRPdmVydmlldyIsImZlX2NmZyI6eyJjaGNsciI6InJnYmEoNjEsIDE4NywgMTU0LCAxKSIsImNtb2RlIjowLCJmY2xyIjoicmdiYSgyNTUsIDI1NSwgMjU1LCAxKSIsImJnIjoicmdiYSgyMSwgMjUsIDMwLCAxKSIsImgiOjM2MCwidyI6MzMwLCJhc3oiOmZhbHNlLCJmdCI6ImRlZmF1bHQiLCJodCI6Im5vbmUiLCJ0cmgiOltdLCJjaGMiOiJyZ2JhKDIzNywgNTAsIDk4LCAxKSIsImJlIjp0cnVlLCJiYyI6InJnYmEoMjU1LDI1NSwyNTUsMC4xKSIsImJ3IjoxLCJjbiI6IiIsImxuZyI6ImVuIiwiaGRpY24iOmZhbHNlLCJ3dG1WIjp7InR5cGUiOiJEYXJxdWJlIiwiZW5hYmxlZCI6dHJ1ZX0sInVjIjoicmdiYSg0OCwgMTMxLCAxMDksIDEpIiwiZGMiOiJyZ2JhKDIzNywgNTAsIDk4LCAxKSIsImFjbyI6WyJJTkRFWEVTIl0sInNtYiI6e30sInNzIjp0cnVlLCJzdCI6dHJ1ZSwic2wiOnRydWUsImN0ZGMiOmZhbHNlfSwiZXhwIjoxNjg3MzQzOTk1LCJzdWIiOiJhY2Nlc3MifQ.U7zXtHxzM0X_p_-ddhK7tqjGJav-NUNG7Gji5ycZHLA"
             id="MarketOverview-5tj3i5t"></iframe> */}
         </div>
+
+        {/* cards */}
+<hr className="my-5"/>
+        <div className="row g-4 py-5 row-cols-1 row-cols-lg-3 px-5 ">
+      <div className="feature col border border-black   ">
+        <div className="feature-icon d-inline-flex align-items-center justify-content-center fs-2 mt-3">
+        <svg
+              id="icon"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
+            </svg>        </div>
+        <h3 className="fs-2">Conocimiento actualizado</h3>
+        <p>Mantente al día con noticias actualizadas y análisis de expertos para tomar decisiones informadas.</p>
+
       </div>
-      <div id="section3" className="d-flex row flex-nowrap overflow-auto">
+      <div className="feature col border border-black  ">
+        <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2  mt-3">
+        <svg
+              id="icon"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <path d="M64 64c0-17.7-14.3-32-32-32S0 46.3 0 64V400c0 44.2 35.8 80 80 80H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H80c-8.8 0-16-7.2-16-16V64zm406.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L320 210.7l-57.4-57.4c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L240 221.3l57.4 57.4c12.5 12.5 32.8 12.5 45.3 0l128-128z" />
+            </svg>        </div>
+        <h3 className="fs-2">Estrategias rentables</h3>
+        <p>Utiliza nuestras señales de entrada para maximizar tus ganancias y reducir tus riesgos.</p>
+
+      </div>
+      <div className="feature col border border-black  ">
+        <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2  mt-3">
+        <svg
+              id="icon"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 640 512"
+            >
+              <path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z" />
+            </svg>
+        </div>
+        <h3 className="fs-2">Experiencia óptima</h3>
+        
+        <p>Diseñado pensando en ti, para ofrecerte una experiencia de usuario intuitiva y sin interrupciones.</p>
+
+      </div>
+    </div>
+
+
+      {/* <div id="section3" className="d-flex row flex-nowrap overflow-auto">
         <div className="card text-bg-primary mb-3" style={{ width: "20rem" }}>
           <div className="card-body">
             <svg
@@ -212,7 +271,7 @@ export const Home = () => {
             </p>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
